ed0ca093a5af6488048b486357f9bb6eadc3c08d
qcacilha <qcacilha@uwo.ca>
2024-08-14 13:08:42 +0000

    Fix: Use GPU instead of CPU for inference

--- inference.py
+++ inference.py
@@ -141,8 +141,7 @@ def run_inference(args):
     elif training_mode == "binaryrelevance":
         labelNames = [br_defect]
 
-    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = torch.device("cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = model.to(device)
 
