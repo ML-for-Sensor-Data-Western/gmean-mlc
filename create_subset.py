5cc7be2d927311ace8c878b0f5de5253a683bc3b
DuminduTraa <dumindutissera@gmail.com>
2024-08-22 23:20:36 +0000

    fix: subset_generation to have mutually exclusive val and test splits

--- create_subset.py
+++ create_subset.py
@@ -3,10 +3,11 @@ import os
 
 image_dir = "/mnt/datassd0/sewer-data/images/" 
 
-df_training = pd.read_csv("/mnt/datassd0/sewer-data/full_dataset_2/SewerML_Train.csv")
-df_val = pd.read_csv("/mnt/datassd0/sewer-data/full_dataset_2/SewerML_Val.csv")
+df_training = pd.read_csv("/mnt/datassd0/sewer-data/annotations2/SewerML_Train.csv")
+df_val = pd.read_csv("/mnt/datassd0/sewer-data/annotations2/SewerML_Val.csv")
 
-'''df_training2 = [] # df_training[os.path.isfile(image_dir + df_training.Filename)]
+''' # Create new dataframes with only the files that exist
+df_training2 = [] # df_training[os.path.isfile(image_dir + df_training.Filename)]
 df_val2 = [] # df_val[os.path.isfile(image_dir + df_val.Filename)]
 
 for index, row in df_val.iterrows():
@@ -29,12 +30,9 @@ df_training2 = pd.DataFrame(df_training2)
 '''
 
 train_subset = df_training.sample(n=400000)
-val_subset = df_val.sample(n=50000)
-test_subset = df_val.sample(n=50000)
+val_subset = df_val.iloc[:50000]
+test_subset = df_val.iloc[50000:100000]
 
-
-
-
-train_subset.to_csv("/mnt/datassd0/sewer-data/training_sub_set/500k_subset/SewerML_Train.csv", index=False)
-val_subset.to_csv("/mnt/datassd0/sewer-data/training_sub_set/500k_subset/SewerML_Val.csv", index=False)
-test_subset.to_csv("/mnt/datassd0/sewer-data/training_sub_set/500k_subset/testing/SewerML_Val.csv", index=False)
+train_subset.to_csv("/mnt/datassd0/sewer-data/annotations_500k/SewerML_Train.csv", index=False)
+val_subset.to_csv("/mnt/datassd0/sewer-data/annotations_500k/SewerML_Val.csv", index=False)
+test_subset.to_csv("/mnt/datassd0/sewer-data/annotations_500k/SewerML_Test.csv", index=False)
