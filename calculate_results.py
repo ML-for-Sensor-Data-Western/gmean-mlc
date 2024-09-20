65656b08d0257e45b26f897b9ece97c8798b9d99
DuminduTraa <dumindutissera@gmail.com>
2024-09-20 20:39:27 +0000

    refactor: calculate_results.py rearrange functions

--- calculate_results.py
+++ calculate_results.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from metrics import evaluation
 
-LabelWeightDict = {
+LABEL_WEIGHT_DICT = {
     "RB": 1.00,
     "OB": 0.5518,
     "PF": 0.2896,
@@ -25,70 +25,40 @@ LabelWeightDict = {
     "OP": 0.3829,
     "OK": 0.4396,
 }
-Labels = list(LabelWeightDict.keys())
-LabelWeights = list(LabelWeightDict.values())
+LABELS = list(LABEL_WEIGHT_DICT.keys())
+LABEL_WEIGHTS = list(LABEL_WEIGHT_DICT.values())
 
 
-def calcualteResults(args):
-    scorePath = args.score_path
-    targetPath = args.gt_path
+def calcualte_results(scores, targets, score_file, args):
+    main_metrics, meta_metrics, class_metrics = evaluation(
+        scores, targets, LABEL_WEIGHTS, threshold=args.threshold
+    )
 
-    split = args.split
+    outputName = "{}_{}_{}".format(args.split, score_file[:-4], args.threshold)
 
-    targetSplitpath = os.path.join(targetPath, "SewerML_{}.csv".format(split))
-    targetsDf = pd.read_csv(targetSplitpath, sep=",")
-    targetsDf = targetsDf.sort_values(by=["Filename"]).reset_index(drop=True)
-    targets = targetsDf[Labels].values
-    
-    for subdir, dirs, files in os.walk(scorePath):
-        print("Iterating in dir: ", subdir)
-        for scoreFile in files:
-            if split.lower() not in scoreFile:
-                continue
-            if (
-                "e2e" not in scoreFile
-                and "twostage" not in scoreFile
-                and "defect" not in scoreFile
-            ):
-                continue
-            if not "sigmoid" in scoreFile:
-                continue
-            if os.path.splitext(scoreFile)[-1] != ".csv":
-                continue
-            print("Calculating results for: ", scoreFile)
-
-            scoresDf = pd.read_csv(os.path.join(subdir, scoreFile), sep=",")
-            scoresDf = scoresDf.sort_values(by=["Filename"]).reset_index(drop=True)
-
-            scores = scoresDf[Labels].values
-
-            main_metrics, meta_metrics, class_metrics = evaluation(
-                scores, targets, LabelWeights, threshold=args.threshold
-            )
-
-            outputName = "{}_{}_{}".format(split, scoreFile[:-4], args.threshold)
-
-            with open(
-                os.path.join(args.score_path, "{}.json".format(outputName)), "w"
-            ) as fp:
-                json.dump(
-                    {
-                        "Highlights": {
-                            "CIW_F2": main_metrics["CIW_F2"],
-                            "NORMAL_F1": meta_metrics["NORMAL_F1"],
-                            "MACRO_F2": main_metrics["MACRO_F2"],
-                            "MACRO_F1": main_metrics["MACRO_F1"],
-                            "DEFECT_F1": meta_metrics["DEFECT_F1"],
-                            },
-                        "Main": main_metrics,
-                        "Meta": meta_metrics,
-                        "Class": class_metrics,
-                        "Labels": Labels,
-                        "LabelWeights": LabelWeights,
-                    },
-                    fp,
-                    indent=4,
-                )
+    with open(
+        os.path.join(args.score_path, "{}.json".format(outputName)),
+        "w",
+        encoding="utf-8",
+    ) as fp:
+        json.dump(
+            {
+                "Highlights": {
+                    "CIW_F2": main_metrics["CIW_F2"],
+                    "NORMAL_F1": meta_metrics["NORMAL_F1"],
+                    "MACRO_F2": main_metrics["MACRO_F2"],
+                    "MACRO_F1": main_metrics["MACRO_F1"],
+                    "DEFECT_F1": meta_metrics["DEFECT_F1"],
+                },
+                "Main": main_metrics,
+                "Meta": meta_metrics,
+                "Class": class_metrics,
+                "Labels": LABELS,
+                "LabelWeights": LABEL_WEIGHTS,
+            },
+            fp,
+            indent=4,
+        )
 
 
 if __name__ == "__main__":
@@ -102,4 +72,35 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    calcualteResults(args)
+    score_path = args.score_path
+    target_path = args.gt_path
+
+    split = args.split
+
+    target_split_path = os.path.join(target_path, "SewerML_{}.csv".format(split))
+    targets_df = pd.read_csv(target_split_path, sep=",")
+    targets_df = targets_df.sort_values(by=["Filename"]).reset_index(drop=True)
+    targets = targets_df[LABELS].values
+
+    for subdir, dirs, files in os.walk(score_path):
+        print("Iterating in dir: ", subdir)
+        for score_file in files:
+            if split.lower() not in score_file:
+                continue
+            if (
+                "e2e" not in score_file
+                and "twostage" not in score_file
+                and "defect" not in score_file
+            ):
+                continue
+            if not "sigmoid" in score_file:
+                continue
+            if os.path.splitext(score_file)[-1] != ".csv":
+                continue
+            print("Calculating results for: ", score_file)
+
+            scores_df = pd.read_csv(os.path.join(subdir, score_file), sep=",")
+            scores_df = scores_df.sort_values(by=["Filename"]).reset_index(drop=True)
+
+            scores = scores_df[LABELS].values
+            calcualte_results(scores, targets, score_file, args)
