d98530b493f7a0a87b8690c4eee72c4a9b2da8b5
DuminduTraa <dumindutissera@gmail.com>
2024-09-25 22:41:51 +0000

    feat: Add multi-threshold result calculation and grid plots

--- calculate_results.py
+++ calculate_results.py
@@ -3,6 +3,7 @@ import json
 import os
 
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from metrics import evaluation
 
@@ -27,6 +28,79 @@ LABEL_WEIGHT_DICT = {
 }
 LABELS = list(LABEL_WEIGHT_DICT.keys())
 LABEL_WEIGHTS = list(LABEL_WEIGHT_DICT.values())
+
+
+def calculate_results_thresholds(scores, targets, score_file, args):
+    num_class = scores.shape[1]
+    
+    thresholds = [i/100 for i in range(5, 100, 5)]
+    macro_f1 = []
+    macro_f2 = []
+    ciw_f2 = []
+    normal_f1 = []
+    defect_f1 = []
+    
+    class_f1 = [[] for i in range(num_class)]
+    class_f2 = [[] for i in range(num_class)]
+    
+    for threshold in thresholds:
+        print("Calculating results with threshold: ", threshold)
+        
+        # Assuming evaluation function is defined elsewhere
+        main_metrics_t, meta_metrics_t, class_metrics_t = evaluation(
+            scores, targets, LABEL_WEIGHTS, threshold=threshold
+        )
+        
+        macro_f1.append(main_metrics_t["MACRO_F1"])
+        macro_f2.append(main_metrics_t["MACRO_F2"])
+        ciw_f2.append(main_metrics_t["CIW_F2"])
+        normal_f1.append(meta_metrics_t["NORMAL_F1"])
+        defect_f1.append(meta_metrics_t["DEFECT_F1"])
+        
+        for class_i in range(num_class):
+            class_f1[class_i].append(class_metrics_t["F1_CLS"][class_i])
+            class_f2[class_i].append(class_metrics_t["F2_CLS"][class_i])
+  
+    fig = plt.figure(figsize=(10, 10))
+    ax1 = fig.add_subplot(2,1,1)
+    ax2 = fig.add_subplot(2,2,3)
+    ax3 = fig.add_subplot(2,2,4)
+    
+    # Plot F1, F2, ciw-F2 in one graph
+    ax1.plot(thresholds, macro_f1, label="Macro F1")
+    ax1.plot(thresholds, macro_f2, label="Macro F2")
+    ax1.plot(thresholds, ciw_f2, label="CIW F2")
+    ax1.plot(thresholds, normal_f1, label="Normal F1")
+    ax1.plot(thresholds, defect_f1, label="Defect F1")
+    
+    ax1.set_xlabel("Threshold")
+    ax1.set_ylabel("Value")
+    ax1.legend()
+    ax1.set_title("Overall Metrics vs Threshold")
+    ax1.set_ylim(0, 1)
+    ax1.grid()
+    
+    for class_i in range(num_class):
+        ax2.plot(thresholds, class_f1[class_i], label=f"F1_{class_i}")
+    ax2.set_xlabel("Threshold")
+    ax2.set_ylabel("Value")
+    ax2.legend()
+    ax2.set_title(f"class F1 vs Threshold")
+    ax2.set_ylim(0, 1)
+    ax2.grid()
+    
+    for class_i in range(num_class):
+        ax3.plot(thresholds, class_f2[class_i], label=f"F2_{class_i}")
+    ax3.set_xlabel("Threshold")
+    ax3.set_ylabel("Value")
+    ax3.legend()
+    ax3.set_title(f"class F2 vs Threshold")
+    ax3.set_ylim(0, 1)
+    ax3.grid()
+    
+    plt.tight_layout()
+    plt.savefig(os.path.join(args.score_path, f"{args.split}_{score_file[:-4]}_metrics.png"))
+    plt.close()
 
 
 def calcualte_results(scores, targets, score_file, args):
@@ -63,12 +137,13 @@ def calcualte_results(scores, targets, score_file, args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--threshold", type=float, default=0.5)
+    parser.add_argument("--gt_path", type=str, default="./annotations")
     parser.add_argument(
         "--split", type=str, default="Val", choices=["Train", "Val", "Test"]
     )
     parser.add_argument("--score_path", type=str, default="./results")
-    parser.add_argument("--gt_path", type=str, default="./annotations")
+    parser.add_argument("--threshold", type=float, default=0.5)
+    parser.add_argument("--multi_threshold", action="store_true")  
 
     args = parser.parse_args()
 
@@ -103,4 +178,7 @@ if __name__ == "__main__":
             scores_df = scores_df.sort_values(by=["Filename"]).reset_index(drop=True)
 
             scores = scores_df[LABELS].values
-            calcualte_results(scores, targets, score_file, args)
+            if args.multi_threshold:
+                calculate_results_thresholds(scores, targets, score_file, args)
+            else:
+                calcualte_results(scores, targets, score_file, args)
