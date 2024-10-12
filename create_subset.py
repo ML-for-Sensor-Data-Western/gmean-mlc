6f432f0b004cb25196b8c49278004ac620884f8a
DuminduTraa <dumindutissera@gmail.com>
2024-10-12 00:52:50 +0000

    feat: skewed defect dataset generation

--- create_subset.py
+++ create_subset.py
@@ -1,30 +1,62 @@
 import pandas as pd
 import os
 
-image_dir = "/mnt/datassd0/sewer-data/images/" 
+IMAGE_DIR = "/mnt/datassd0/sewer-data/images/"
+TRAIN_ANN = "/mnt/datassd0/sewer-data/annotations/SewerML_Train.csv"
+VAL_ANN = "/mnt/datassd0/sewer-data/annotations/SewerML_Val.csv"
 
-df_training = pd.read_csv("/mnt/datassd0/sewer-data/annotations/SewerML_Train.csv")
-df_val = pd.read_csv("/mnt/datassd0/sewer-data/annotations/SewerML_Val.csv")
-df_test = pd.read_csv("/mnt/datassd0/sewer-data/annotations/SewerML_Test.csv")
+NUM_DEFECT = 50000
+DEFECT_TO_NORMAL_RATIO = 10
+TRAIN_SPLIT, VAL_SPLIT, TEST_SPLIT = 0.8, 0.1, 0.1
 
-num_images = len(os.listdir(image_dir))
 
-num_training = len(df_training)
-num_val = len(df_val)
-# num_test = len(df_test)
+num_train_defect = int(NUM_DEFECT * TRAIN_SPLIT)
+num_val_defect = int(NUM_DEFECT * VAL_SPLIT)
+num_test_defect = int(NUM_DEFECT * TEST_SPLIT)
+num_train_normal = num_train_defect * DEFECT_TO_NORMAL_RATIO
+num_val_normal = num_val_defect * DEFECT_TO_NORMAL_RATIO
+num_test_normal = num_test_defect * DEFECT_TO_NORMAL_RATIO
 
-num_data = num_training + num_val# + num_test
+OUTPUT_DIR = f"/mnt/datassd0/sewer-data/defect_{int(NUM_DEFECT/1000)}k_1_to_{DEFECT_TO_NORMAL_RATIO}"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-print("Number of files in image_dir: ", num_images)
+df_train = pd.read_csv(TRAIN_ANN)
+df_val = pd.read_csv(VAL_ANN)
 
-print("\nNumber of images in sheets: ", num_data)
-print("Number of training images: ", num_training)
-print("Number of validation images: ", num_val)
-# print("Number of test images: ", num_test)
+df_train_defect = df_train[df_train["Defect"] == 1]
+df_train_normal = df_train[df_train["Defect"] == 0]
 
-print("\nNumber of missing files: ", num_data - num_images)
+df_val_defect = df_val[df_val["Defect"] == 1]
+df_val_normal = df_val[df_val["Defect"] == 0]
 
-'''# Check missing files and write filename to txt file
+# Create a subset of the dataframes
+# Create train subset
+train_subset_defect = df_train_defect.sample(n=num_train_defect, random_state=1)
+train_subset_normal = df_train_normal.sample(n=num_train_normal, random_state=1)
+train_subset = pd.concat([train_subset_defect, train_subset_normal])
+
+# val and test subsets from original val set
+df_val_defect_shuffled = df_val_defect.sample(frac=1, random_state=1)
+df_val_normal_shuffled = df_val_normal.sample(frac=1, random_state=1)
+
+val_subset_defect = df_val_defect_shuffled.iloc[:num_val_defect]
+val_subset_normal = df_val_normal_shuffled.iloc[:num_val_normal]
+val_subset = pd.concat([val_subset_defect, val_subset_normal])
+
+test_subset_defect = df_val_defect_shuffled.iloc[
+    num_val_defect : num_val_defect + num_test_defect
+]
+test_subset_normal = df_val_normal_shuffled.iloc[
+    num_val_normal : num_val_normal + num_test_normal
+]
+test_subset = pd.concat([test_subset_defect, test_subset_normal])
+
+train_subset.to_csv(os.path.join(OUTPUT_DIR, "SewerML_Train.csv"), index=False)
+val_subset.to_csv(os.path.join(OUTPUT_DIR, "SewerML_Val.csv"), index=False)
+test_subset.to_csv(os.path.join(OUTPUT_DIR, "SewerML_Test.csv"), index=False)
+
+
+"""# Check missing files and write filename to txt file
 missing_filenames = []
 
 for df in [df_training, df_val]:
@@ -38,9 +70,9 @@ print("Number of missing files from search:", len(missing_filenames))
 with open("missing_filenames.txt", "w") as f:
     for filename in missing_filenames:
         f.write(filename + "\n")
-'''
+"""
 
-''' # Create new dataframes with only the files that exist
+""" # Create new dataframes with only the files that exist
 df_training2 = [] # df_training[os.path.isfile(image_dir + df_training.Filename)]
 df_val2 = [] # df_val[os.path.isfile(image_dir + df_val.Filename)]
 
@@ -60,14 +92,4 @@ for index, row in df_training.iterrows():
             df_training2.append(row)
 
 df_training2 = pd.DataFrame(df_training2)
-'''
-
-# Create a subset of the dataframes
-train_subset = df_training.sample(n=400000, random_state=1)
-df_val_shuffled = df_val.sample(frac=1, random_state=1)
-val_subset = df_val_shuffled.iloc[:50000]
-test_subset = df_val_shuffled.iloc[50000:100000]
-
-train_subset.to_csv("/mnt/datassd0/sewer-data/annotations_500k/SewerML_Train.csv", index=False)
-val_subset.to_csv("/mnt/datassd0/sewer-data/annotations_500k/SewerML_Val.csv", index=False)
-test_subset.to_csv("/mnt/datassd0/sewer-data/annotations_500k/SewerML_Test.csv", index=False)
+"""
