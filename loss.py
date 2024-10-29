9a420936450cd0db5ee4582ffe3d04fcf5adfddf
DuminduTraa <dumindutissera@gmail.com>
2024-10-29 06:54:47 +0000

    feat: add focal loss

--- loss.py
+++ loss.py
@@ -9,13 +9,63 @@ class HybridLoss(torch.nn.Module):
         class_counts: Optional[torch.Tensor] = None,
         defect_count: Optional[torch.Tensor] = None,
         beta: float = 0.9999,
+        base_loss: Literal["sigmoid", "focal"] = "focal",
+        focal_gamma: float = 2.0,
         meta_loss_weight: float = 1.0,
         push_mode: Literal["positive_push", "all_push"] = "positive_push",
     ):
         super().__init__()
         self.defect_class_weights = self._get_class_weights(class_counts, beta)
+        self.base_loss = base_loss
+        self.focal_gamma = focal_gamma
         self.meta_loss_weight = meta_loss_weight
         self.push_mode = push_mode
+        
+        if base_loss not in ["sigmoid", "focal"]:
+            raise ValueError(f"Invalid base_loss '{base_loss}'")
+        if push_mode not in ["positive_push", "all_push"]:
+            raise ValueError(f"Invalid push_mode '{push_mode}'")
+
+    @staticmethod
+    def _focal_loss(
+        logits: torch.Tensor,
+        targets: torch.Tensor,
+        alpha: Optional[torch.Tensor] = None,
+        gamma: float = 2.0,
+    ) -> float:
+        """
+        Calculate the focal loss for a batch of predictions and targets.
+        Focal loss = -alpha_t * (1-pt)^gamma * log(pt)
+        pt is the probability of being classified to the true class.
+        pt = p (if true class), otherwise pt = 1 - p. p = sigmoid(logit).
+        Args:
+            logits (torch.Tensor): logit scores for each class (batch_size, num_classes).
+            targets (torch.Tensor): ground truth binary labels for each class (batch_size, num_classes).
+            alpha (torch.Tensor): weights for each class to handle class imbalance (num_classes).
+            gamma (float): A hyperparameter that controls the weighting scheme.
+        Returns:
+            float: focal_loss: A float32 scalar representing normalized total loss..
+        """
+        bc_loss = F.binary_cross_entropy_with_logits(logits, targets, reduction="none")
+
+        if gamma == 0.0:
+            modulator = 1.0
+        else:
+            modulator = torch.exp(
+                -gamma * targets * logits
+                - gamma * torch.log(1 + torch.exp(-1.0 * logits))
+            )
+
+        loss = modulator * bc_loss
+
+        if alpha is not None:
+            weighted_loss = alpha * loss
+            focal_loss = torch.sum(weighted_loss)
+        else:
+            focal_loss = torch.sum(loss)
+
+        focal_loss /= (torch.sum(targets) + 1e-6)
+        return focal_loss
 
     @staticmethod
     def _get_class_weights(class_counts: torch.Tensor, beta: float) -> torch.Tensor:
@@ -55,7 +105,7 @@ class HybridLoss(torch.nn.Module):
 
     def _calculate_multi_label_loss(
         self, logits: torch.Tensor, targets: torch.Tensor, class_weights: torch.Tensor
-    ) -> torch.Tensor:
+    ) -> float:
         """
         Calculate the multi-label loss for a batch of predictions and targets.
         Args:
@@ -63,20 +113,24 @@ class HybridLoss(torch.nn.Module):
             targets (torch.Tensor): ground truth binary labels for each class (batch_size, num_classes).
             class_weights (torch.Tensor): weights for each class to handle class imbalance (num_classes).
         Returns:
-            torch.Tensor: The calculated loss for each instance in the batch (batch_size, num_classes).
+            float: The mean multi-label loss for the batch.
         """
         weights = self._calculate_balancing_weights(class_weights, targets)
-
-        defect_type_loss = F.binary_cross_entropy_with_logits(
-            logits, targets, weights, reduction="none"
-        )  # (batch_size, num_classes)
+        
+        if self.base_loss == "sigmoid":
+            defect_type_loss = F.binary_cross_entropy_with_logits(
+                logits, targets, weights, reduction="mean"
+            ) 
+        else:
+            defect_type_loss = self._focal_loss(
+                logits, targets, weights, self.focal_gamma
+            )
 
         return defect_type_loss
 
-    @staticmethod
-    def _calculate_meta_loss(
-        logits: torch.Tensor, targets: torch.Tensor
-    ) -> torch.Tensor:
+    def _calculate_positive_push_meta_loss(
+        self, logits: torch.Tensor, targets: torch.Tensor
+    ) -> float:
         """
         Calculate the target-push meta loss for defect detection.
         the meta positive node consists only of the nodes for the defect that are present in the image.
@@ -85,11 +139,9 @@ class HybridLoss(torch.nn.Module):
             logits (torch.Tensor): The predicted logits from the model with shape (batch_size, num_classes).
             targets (torch.Tensor): The ground truth targets with shape (batch_size, num_classes).
         Returns:
-            torch.Tensor: The calculated defect loss with shape (batch_size, 1).
+            float: Average defect loss for the batch
         """
-        meta_targets = torch.sum(targets, 1, True).clamp(
-            0, 1
-        )  # (batch_size, 1) 0 or 1
+        meta_targets = torch.sum(targets, 1, True).clamp(0, 1)  # (batch_size, 1) 0 or 1
 
         # copy target to a new tensor
         meta_logit_weights = targets.clone()
@@ -100,16 +152,20 @@ class HybridLoss(torch.nn.Module):
             logits * meta_logit_weights, dim=1, keepdim=True
         ) / torch.sum(meta_logit_weights, dim=1, keepdim=True)
 
-        meta_loss = F.binary_cross_entropy_with_logits(
-            meta_logits, meta_targets, reduction="none"
-        )  # (batch_size, 1)
+        if self.base_loss == "sigmoid":
+            meta_loss = F.binary_cross_entropy_with_logits(
+                meta_logits, meta_targets, reduction="mean"
+            ) 
+        else:
+            meta_loss = self._focal_loss(
+                meta_logits, meta_targets, gamma=self.focal_gamma
+            )
 
         return meta_loss
 
-    @staticmethod
     def _calculate_all_push_meta_loss(
-        logits: torch.Tensor, targets: torch.Tensor
-    ) -> torch.Tensor:
+        self, logits: torch.Tensor, targets: torch.Tensor
+    ) -> float:
         """
         Calculate the all-push meta loss for defect detection.
         the meta positive node consists of all the nodes in the image.
@@ -118,17 +174,20 @@ class HybridLoss(torch.nn.Module):
             logits (torch.Tensor): logits from the model with shape (batch_size, num_classes).
             targets (torch.Tensor): ground truth targets with shape (batch_size, num_classes).
         Returns:
-            torch.Tensor: The calculated defect loss with shape (batch_size, 1).
+            float: Average defect loss for the batch
         """
-        meta_targets = torch.sum(targets, 1, True).clamp(
-            0, 1
-        )  # (batch_size, 1) 0 or 1
+        meta_targets = torch.sum(targets, 1, True).clamp(0, 1)  # (batch_size, 1) 0 or 1
 
         meta_logits = torch.mean(logits, dim=1, keepdim=True)
 
-        meta_loss = F.binary_cross_entropy_with_logits(
-            meta_logits, meta_targets, reduction="none"
-        )  # (batch_size, 1)
+        if self.base_loss == "sigmoid":
+            meta_loss = F.binary_cross_entropy_with_logits(
+                meta_logits, meta_targets, reduction="mean"
+            )
+        else:
+            meta_loss = self._focal_loss(
+                meta_logits, meta_targets, gamma=self.focal_gamma
+            )
 
         return meta_loss
 
@@ -138,14 +197,11 @@ class HybridLoss(torch.nn.Module):
         )
 
         if self.push_mode == "positive_push":
-            meta_loss = self._calculate_meta_loss(logits, targets)
+            meta_loss = self._calculate_positive_push_meta_loss(logits, targets)
         else:
             meta_loss = self._calculate_all_push_meta_loss(logits, targets)
 
-        final_loss = torch.mean(
-            torch.mean(defect_type_loss, 1, keepdim=True)
-            + self.meta_loss_weight * meta_loss
-        )
+        final_loss = defect_type_loss + self.meta_loss_weight * meta_loss
 
         return final_loss
 
@@ -161,12 +217,12 @@ if __name__ == "__main__":
 
     logits = torch.rand(10, NUM_CLASSES).float()
 
-    labels = torch.rand(10, NUM_CLASSES).float()
-    labels[labels > 0.5] = 1.0
-    labels[labels <= 0.5] = 0.0
+    targets = torch.rand(10, NUM_CLASSES).float()
+    targets[targets > 0.5] = 1.0
+    targets[targets <= 0.5] = 0.0
 
     print("\nLogits: ", logits)
-    print("\nLabels: ", labels)
+    print("\nLabels: ", targets)
 
     criterion = HybridLoss(
         class_counts=CLASS_COUNTS,
@@ -175,5 +231,5 @@ if __name__ == "__main__":
         push_mode=PUSH_MODE,
     )
 
-    loss = criterion(logits, labels)
+    loss = criterion(logits, targets)
     print("\nLoss: ", loss)
