7d8b5732d79cd26ab91bda3a7ad79e0c88f0de61
DuminduTraa <dumindutissera@gmail.com>
2024-12-18 22:48:24 +0000

    feat: update tuner with custom tunable params

--- lightning_tuner.py
+++ lightning_tuner.py
@@ -2,7 +2,6 @@ import os
 from argparse import ArgumentParser
 
 import pytorch_lightning as pl
-import torch
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from ray import tune
@@ -17,6 +16,27 @@ from lightning_datamodules import (
     MultiLabelDataModule,
 )
 from lightning_model import MultiLabelModel
+from loss import HybridLoss
+
+
+"""
+The parameters in GLOBAL_CONFIG are the total hyperparameters that can be tuned.
+The same parameters are in terminal args as well. 
+Make sure the parameters which do not need to be tuned are properly defined in 
+terminal args and the parameters to be tuned (must be a subset of GLOBAL_CONFIG) 
+are stated in args.params.
+"""
+
+GLOBAL_CONFIG = {
+    "batch_size": tune.choice([64, 128, 256]),
+    "learning_rate": tune.choice([0.001, 0.01, 0.02, 0.03, 0.05, 0.075, 0.1]),
+    "lr_decay": tune.choice([0.01, 0.05, 0.1]),
+    "momentum": tune.choice([0.5, 0.6, 0.7, 0.8, 0.9]),
+    "weight_decay": tune.uniform(0.0001,0.01),
+    "class_balancing_beta": tune.choice([0.9, 0.99, 0.999, 0.9999]),
+    "meta_loss_weight": tune.uniform(0.0, 1.0),
+    "meta_loss_beta": tune.uniform(0.0, 1.0),
+}
 
 
 # had to redefine because of value erro: Expected a parent
@@ -34,6 +54,17 @@ class CustomLogger(TensorBoardLogger):
 
 def main(config, args):
     # pl.seed_everything(1234567890)
+    
+    args_dict = vars(args) 
+       
+    hyperparameters = {}
+    for hp_name in GLOBAL_CONFIG.keys():
+        if hp_name in config.keys():
+            hyperparameters[hp_name] = config[hp_name]
+        elif hp_name in args_dict.keys():
+            hyperparameters[hp_name] = args_dict[hp_name]
+        else:
+            raise Exception(f"Hyperparam underfined in args '{hp_name}'")
 
     # Init data with transforms
     img_size = 299 if args.model in ["inception_v3", "chen2018_multilabel"] else 224
@@ -60,7 +91,7 @@ def main(config, args):
 
     if args.training_mode == "e2e":
         dm = MultiLabelDataModule(
-            batch_size=config["batch_size"],
+            batch_size=hyperparameters["batch_size"],
             workers=args.workers,
             ann_root=args.ann_root,
             data_root=args.data_root,
@@ -70,7 +101,7 @@ def main(config, args):
         )
     elif args.training_mode == "defect":
         dm = MultiLabelDataModule(
-            batch_size=config["batch_size"],
+            batch_size=hyperparameters["batch_size"],
             workers=args.workers,
             ann_root=args.ann_root,
             data_root=args.data_root,
@@ -80,7 +111,7 @@ def main(config, args):
         )
     elif args.training_mode == "binary":
         dm = BinaryDataModule(
-            batch_size=config["batch_size"],
+            batch_size=hyperparameters["batch_size"],
             workers=args.workers,
             ann_root=args.ann_root,
             data_root=args.data_root,
@@ -94,19 +125,25 @@ def main(config, args):
     dm.setup("fit")
 
     # Init our model
-    criterion = torch.nn.BCEWithLogitsLoss(pos_weight=dm.class_weights)
+    criterion = HybridLoss(
+        class_counts=dm.class_counts,
+        normal_count=dm.num_train_samples - dm.defect_count,
+        class_balancing_beta=hyperparameters["class_balancing_beta"],
+        base_loss=args.base_loss,
+        focal_gamma=args.focal_gamma,
+        meta_loss_weight=hyperparameters["meta_loss_weight"],
+        meta_loss_beta=hyperparameters["meta_loss_beta"],
+    )
 
     light_model = MultiLabelModel(
         model=args.model,
         num_classes=dm.num_classes,
         criterion=criterion,
-        learning_rate=config["learning_rate"],
-        momentum=config["momentum"],
-        weight_decay=config["weight_decay"],
-        batch_size=config["batch_size"],
+        learning_rate=hyperparameters["learning_rate"],
+        momentum=hyperparameters["momentum"],
+        weight_decay=hyperparameters["weight_decay"],
+        batch_size=hyperparameters["batch_size"],
         lr_steps=args.lr_steps,
-        dropout=config["dropout"],
-        attention_dropout=config["attention_dropout"],
     )
 
     # train
@@ -118,25 +155,12 @@ def main(config, args):
         version=prefix + "version_" + str(args.log_version),
     )
 
-    logger_path = os.path.join(
-        args.log_save_dir, args.model, prefix + "version_" + str(args.log_version)
-    )
-
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=logger_path,
-        filename="{epoch:02d}-{val_acc:.2f}",
-        save_top_k=3,
-        save_last=True,
-        verbose=False,
-        monitor="val_acc",
-        mode="max",
-    )
 
     tune_callback = MyTuneReportCheckpointCallback(
-        metrics={"val_acc": "val_acc"}, on="validation_end"
+        metrics={args.metric: args.metric}, on="validation_end"
     )
 
-    lr_monitor = LearningRateMonitor(logging_interval="step")
+    lr_monitor = LearningRateMonitor(logging_interval="epoch")
 
     trainer = pl.Trainer(
         num_nodes=1,
@@ -144,7 +168,7 @@ def main(config, args):
         max_epochs=args.max_epochs,
         benchmark=True,
         logger=logger,
-        callbacks=[checkpoint_callback, lr_monitor, tune_callback],
+        callbacks=[lr_monitor, tune_callback],
         enable_progress_bar=False,
     )
 
@@ -168,22 +192,35 @@ def run_cli():
     parser.add_argument("--max_concurrent_trials", type=int, default=1)
     parser.add_argument("--cpus_per_trial", type=int, default=4)
     parser.add_argument("--gpus_per_trial", type=int, default=1)
-    parser.add_argument("--log_save_dir", type=str, default="./logs")
-    parser.add_argument("--log_version", type=int, default=1)
     parser.add_argument(
         "--training_mode",
         type=str,
         default="e2e",
         choices=["e2e", "binary", "defect"],
     )
-    # Trainer args
     parser.add_argument("--precision", type=int, default=32, choices=[16, 32])
-    parser.add_argument("--max_epochs", type=int, default=50)
-    parser.add_argument("--lr_steps", nargs="+", type=int, default=[15, 30, 40])
-    # Model args
+    parser.add_argument("--max_epochs", type=int, default=40)
+    parser.add_argument("--learning_rate", type=float, default=0.1)
+    parser.add_argument("--momentum", type=float, default=0.9)
+    parser.add_argument("--lr_steps", nargs="+", type=int, default=[20, 30])
+    parser.add_argument("--lr_decay", type=float, default=0.01)
+    parser.add_argument("--weight_decay", type=float, default=0.0001)
+    parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument(
         "--model", type=str, default="resnet18", choices=MultiLabelModel.MODEL_NAMES
     )
+    parser.add_argument("--mtl_heads", action="store_true")
+    parser.add_argument(
+        "--base_loss", type=str, default="focal", choices=["focal", "bce"]
+    )
+    parser.add_argument("--focal_gamma", type=float, default=2.0)
+    parser.add_argument("--class_balancing_beta", type=float, default=0.9999)
+    parser.add_argument("--meta_loss_weight", type=float, default=0.1)
+    parser.add_argument("--meta_loss_beta", type=float, default=0.1)
+    parser.add_argument("--metric", type=str, help="metric to optimizer", choices=["val_ap", "val_f1", "val_f2"])
+    parser.add_argument("--params", nargs="+", type=str, help="params to optimize", default=["meta_loss_weight", "meta_loss_beta", "class_balancing_beta"])
+    parser.add_argument("--log_save_dir", type=str, default="./logs")
+    parser.add_argument("--log_version", type=int, default=1)
 
     args = parser.parse_args()
     print(args)
@@ -191,22 +228,24 @@ def run_cli():
     # Adjust learning rate to amount of GPUs
     # args.workers = max(0, min(8, 4 * len(args.gpus_per_trial)))
     # args.learning_rate = args.learning_rate * (len(args.gpus_per_trial) * args.batch_size) / 256
-
-    config = {
-        "batch_size": tune.choice([64, 128, 256]),
-        "learning_rate": tune.choice([0.001, 0.005, 0.01, 0.02, 0.03, 0.05, 0.075, 0.1]),
-        "momentum": tune.choice([0.5, 0.6, 0.7, 0.8, 0.9]),
-        "weight_decay": tune.uniform(0.0001,0.1),
-        "dropout": tune.uniform(0.1, 0.5),
-        "attention_dropout": tune.uniform(0.05, 0.3),
-    }
     
-    search_algo = OptunaSearch(metric="val_acc", mode="max")
+    config = {}
+    for param in args.params:
+        if param in GLOBAL_CONFIG.keys():
+            config[param] = GLOBAL_CONFIG[param]
+        else:
+            raise Exception(f"Hyperparam underfined in global config '{param}'")
+    
+    print("tunable parameters: ", config)
+    
+    metric_optim_mode = "max" if args.metric in ["val_ap", "val_f1", "val_f2"] else "min"
+    
+    search_alg = OptunaSearch(metric=args.metric, mode=metric_optim_mode)
 
     search_scheduler = ASHAScheduler(
         time_attr="training_iteration", # default
-        metric="val_acc",
-        mode="max",
+        metric=args.metric,
+        mode=metric_optim_mode,
         max_t=100, # default
         grace_period=18,
         reduction_factor=4, # default
@@ -215,8 +254,8 @@ def run_cli():
     )
 
     reporter = CLIReporter(
-        parameter_columns=["batch_size", "learning_rate", "momentum", "weight_decay", "dropout", "attention_dropout"],
-        metric_columns=["val_acc", "training_iteration"],
+        parameter_columns=args.params,
+        metric_columns=[args.metric, "training_iteration"],
         print_intermediate_tables=False,
     )
 
@@ -224,7 +263,7 @@ def run_cli():
         tune.with_parameters(main, args=args),
         resources_per_trial={"cpu": args.cpus_per_trial, "gpu": args.gpus_per_trial},
         config=config,
-        search_alg=search_algo,
+        search_alg=search_alg,
         scheduler=search_scheduler,
         num_samples=args.num_trials,
         max_concurrent_trials=args.max_concurrent_trials,
@@ -237,7 +276,7 @@ def run_cli():
 
     print(
         "The best param values are: ",
-        analysis.get_best_config(metric="val_acc", mode="max"),
+        analysis.get_best_config(metric=args.metric, mode=metric_optim_mode),
     )
 
 
